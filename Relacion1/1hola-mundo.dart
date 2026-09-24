06ecac10dbe0e6b69f4dab4d58b0a0a51df529b2
Rubén Rivas <rrivflo1209@g.educaand.es>
2026-09-24 19:26:38 +0200

    ejercicio uno, probando dart

---
+++ Relacion1/1hola-mundo.dart
@@ -0,0 +1,12 @@
+/* Ejercicio 1 de la relación 1
+    Archivo: 1hola-mundo.dart
+    Fecha Creación: septiembre 2026
+    autor: Rubén Rivas */
+
+void main(){
+    print ("Hola mundo!");
+    String nombre = "Ruben";
+    int veces = 1;
+    print("Hola $nombre!");
+    print("Has ejecutado el programa ${veces*2-1} vez(es)");
+}
